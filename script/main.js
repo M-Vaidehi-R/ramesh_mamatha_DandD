42976c52a143aab7f8cfe9b77bf903ca01d61632
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-02-08 09:19:10 -0500

    drag and drop success:)

--- script/main.js
+++ script/main.js
@@ -1,8 +1,12 @@
 (() => {
 	//collect the buttons at the bottom of the jpgImage
 	let theThumbnails = document.querySelectorAll("#buttonHolder img");
-       gameBoard=document.querySelector(".puzzle-board");
+       gameBoard=document.querySelector(".puzzle-board"),
+			 puzzlePieces=document.querySelectorAll(".puzzle-pieces img"),
+			 dropZones=document.querySelectorAll(".drop-zone");
 
+
+//theThumbnails, gameBoard,puzzlePieces,dropZones are variables
 
 // theThumbnailscollects all of the image elements into  an array-like container that looks like this:
     /*
@@ -19,9 +23,41 @@ function changeBgImg() {
 
 	gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
 }
+
+function dragStarted(event) {
+	console.log("started dragging a piece");
+	// use the setData method of the drag event to store a reference to the current element
+event.dataTransfer.setData("currentItem", event.target.id);
+}
+
+function allowDragover(event) {
+	event.preventDefault();  //turn off default behaviour
+	console.log ("dragged over me");
+	// retrieve the dragged element using the Datatransfer object
+	//this was set in the  drag event using the SetData method
+	let droppedEl=event.dataTransfer.getData("currentItem");
+	console.log(droppedEl);
+}
+
+function allowDrop(event) {
+	event.preventDefault();  //turn off default behaviour----follow our instructions instead of what the browser would do on drop
+	console.log ("dropped on me");
+
+
+let droppedEl = event.dataTransfer.getData("currentItem");
+console.log(droppedEl);
+this.appendChild(document.querySelector(`#${droppedEl}`));
+}
+
+
 	//add event handling here -> loop through theThumbnails array and add event handling to each image
  theThumbnails.forEach( item => item.addEventListener("click", changeBgImg));
+ // listen for the dragstarted event on the puzzle puzzlePieces
+ puzzlePieces.forEach(piece => piece.addEventListener("dragstart", dragStarted));
 
-
-
+ //add event handling for the drop zones (dragover and drop)
+ dropZones.forEach(zone => {
+	 zone.addEventListener("dragover", allowDragover);
+	  zone.addEventListener("drop", allowDrop);
+ })
 })();
