6be29260c646ce210292356bc2eccc1dc24d6ffa
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-03-04 10:11:13 -0500

    only one element can be dropped- 1st bug resolved

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 (() => {
 	//collect the buttons at the bottom of the jpgImage
-	let theThumbnails = document.querySelectorAll("#buttonHolder img");
+	let theThumbnails = document.querySelectorAll("#buttonHolder img"),     
        gameBoard=document.querySelector(".puzzle-board"),
-			 puzzlePieces=document.querySelectorAll(".puzzle-pieces img"),
-			 dropZones=document.querySelectorAll(".drop-zone");
+		puzzlePieces=document.querySelectorAll(".puzzle-pieces img"),
+		dropZones=document.querySelectorAll(".drop-zone");
 
 
 //theThumbnails, gameBoard,puzzlePieces,dropZones are variables
@@ -16,12 +16,21 @@
 			<img src="images/bottomRight0.jpg" class="puzzle-image" alt="bottom right">
 		*/
 
-function changeBgImg() {
+		const puzzlePaths=["topLeft", "topRight", "bottomLeft", "bottomRight"]   //this is an array :)
+
+			//add a line where u remove all the child elements of the puzzle board.....use remove-child prop
+
+function changeImgSet() {
+
 
 //the 'this' keyword refers to the element that triggers  this function (the nav button we click with the custom data attribute of bgref)
 	//debugger;
 
-	gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
+	gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;  //game boarde lle style lle backgroundImage        $ specifies that end-numbers in image name, 
+	//loop through all of the small draggable images and change their src attribute with js
+	puzzlePaths.forEach((img, index) => {
+		puzzlePieces[index].src =`images/${img + this.dataset.bgref}.jpg`;
+	}); //update each image's src one at a time)
 }
 
 function dragStarted(event) {
@@ -43,21 +52,31 @@ function allowDrop(event) {
 	event.preventDefault();  //turn off default behaviour----follow our instructions instead of what the browser would do on drop
 	console.log ("dropped on me");
 
-
+	if (this.childElementCount > 0)
+	   {
+		   return;
+	   }
+   
+	else {   
 let droppedEl = event.dataTransfer.getData("currentItem");
 console.log(droppedEl);
 this.appendChild(document.querySelector(`#${droppedEl}`));
+	}
 }
 
 
+// end of declaring all functions.....now its calling time :]
+
+
 	//add event handling here -> loop through theThumbnails array and add event handling to each image
- theThumbnails.forEach( item => item.addEventListener("click", changeBgImg));
+ theThumbnails.forEach( item => item.addEventListener("click", changeImgSet));      //thumbnails lle oro item, when clicked, call changeImgSet function, its for 
  // listen for the dragstarted event on the puzzle puzzlePieces
- puzzlePieces.forEach(piece => piece.addEventListener("dragstart", dragStarted));
+ puzzlePieces.forEach(piece => piece.addEventListener("dragstart", dragStarted));    //dragstart is a prop               puzzlePieces(inside that verticle box- 4 imgs), when its dragged from there, dragStarted function is called.
 
  //add event handling for the drop zones (dragover and drop)
- dropZones.forEach(zone => {
+ dropZones.forEach(zone => {                                    
+	 // for each zone in dropzone, if element is dragged over an element(dragover- keyword), call allowDragover func else if its dropped in the zone, call allowdrop func
 	 zone.addEventListener("dragover", allowDragover);
-	  zone.addEventListener("drop", allowDrop);
+	 zone.addEventListener("drop", allowDrop);
  })
 })();
